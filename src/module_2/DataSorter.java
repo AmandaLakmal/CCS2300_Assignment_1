2ef832549ad1be0853605843cc8a7462b211ef9f
AmandaLakmal <lakmalamanda@gmail.com>
2026-02-24 21:57:20 +0530

    Add DataSorter class and  main menu loop

--- src/module_2/DataSorter.java
+++ src/module_2/DataSorter.java
@@ -1,1 +1,22 @@
+package module_2;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
 
+public class DataSorter {
+    public void runModule(){
+        Scanner scanner = new Scanner(System.in);
+        boolean runnig = true;
+
+        while(runnig){
+            System.out.println("\n=== Module 2: Sorting Algorithm Comparison Tool ===");
+            System.out.println("1. Enter number manually");
+            System.out.println("2. Generate random numbers");
+            System.out.println("3. Return to main menu / Exit");
+            System.out.println("Choose an option : ");
+
+            int choice = scanner.nextInt();
+            int[] data = null;
+        }
+    }
+}
