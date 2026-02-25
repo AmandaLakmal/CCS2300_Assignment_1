bf15796b106b17170470ab574e7276f44bb1264c
Maheen <maheen.sayuru21@gmail.com>
2026-02-25 18:32:43 +0530

    Rename main.java to Main.java to follow Java naming conventions

--- src/main.java
+++ src/main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import module_3.LocationBST;
 import module_3.PerformanceAnalyzer;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
