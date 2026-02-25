1ebba69a9893f122eba29cd8de7d0ab5ca052ca2
AmandaLakmal <lakmalamanda@gmail.com>
2026-02-25 07:38:50 +0530

    Implement QuickSort algorithm with execution time measurement

---
+++ src/module_2/QuickSort.java
@@ -0,0 +1,36 @@
+package module_2;
+
+public class QuickSort {
+    public static long sortAndMeasure(int[] arr) {
+        int[] data = arr.clone();
+        long startTime = System.nanoTime();
+        quickSort(data, 0, data.length - 1);
+        long endTime = System.nanoTime();
+        return endTime - startTime;
+    }
+
+    private static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+}
