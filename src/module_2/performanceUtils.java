0fc4017c3c96283d62d2fe1a295d5674e1682f60
AmandaLakmal <lakmalamanda@gmail.com>
2026-02-25 09:18:00 +0530

    Implement performance comparison table utility

--- src/module_2/performanceUtils.java
+++ src/module_2/performanceUtils.java
@@ -2,15 +2,15 @@ package module_2;
 
 public class performanceUtils {
     public static void displayComparisonTable(long bubbleTime, long mergeTime, long quickTime) {
-        System.out.println("\n======================================================");
+        System.out.println("\n=======================================================");
         System.out.println("             PERFORMANCE COMPARISON TABLE             ");
-        System.out.println("======================================================");
+        System.out.println("=======================================================");
         System.out.printf("%-25s | %-25s%n", "Sorting Algorithm", "Execution Time (ns)");
-        System.out.println("------------------------------------------------------");
+        System.out.println("-------------------------------------------------------");
         System.out.printf("%-25s | %-25d%n", "Bubble Sort", bubbleTime);
         System.out.printf("%-25s | %-25d%n", "Merge Sort", mergeTime);
         System.out.printf("%-25s | %-25d%n", "Quick Sort", quickTime);
-        System.out.println("======================================================");
+        System.out.println("=======================================================");
 
         // Highlight the fastest algorithm
         long minTime = Math.min(bubbleTime, Math.min(mergeTime, quickTime));
@@ -20,6 +20,6 @@ public class performanceUtils {
         else fastest = "Quick Sort";
 
         System.out.println("Fastest Algorithm is: " + fastest);
-        System.out.println("======================================================\n");
+        System.out.println("=======================================================\n");
     }
 }
