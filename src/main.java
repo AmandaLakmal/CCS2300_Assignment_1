4dadb56797cb4563c512da2613301bf490818c4c
cit-24-02-0058-Ruhini <cit-24-02-0058@sltc.ac.lk>
2026-02-26 20:10:40 +0530

    change main

--- src/main.java
+++
@@ -1,8 +0,0 @@
-import module_1.RoutePlanner;
-
-public class Main {
-    public static void main(String[] args) {
-        RoutePlanner planner = new RoutePlanner();
-        planner.start();
-    }
-}
