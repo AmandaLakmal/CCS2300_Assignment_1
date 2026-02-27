ba86656d9c56f7eff59916a4e6bdf4fa58e9f4a2
cit-24-02-0058-Ruhini <cit-24-02-0058@sltc.ac.lk>
2026-02-27 10:06:33 +0530

    create LocationNode class for BST structure

--- src/Module_1/LocationNode.java
+++ src/Module_1/LocationNode.java
@@ -1,1 +1,13 @@
+package Module_1;
 
+public class LocationNode {
+    String locationName;
+    LocationNode left;
+    LocationNode right;
+
+    public LocationNode(String locationName) {
+        this.locationName = locationName;
+        this.left = null;
+        this.right = null;
+    }
+}
